dee39807946b8662ef639968714d8ad0ed6734ee
busragelik@gmail.com <busragelik@gmail.com>
2021-06-17 10:32:24 +0300

    .

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Icon, Menu, Table } from 'semantic-ui-react'
 import ProductService from "../services/productService";
+import { Link } from "react-router-dom";
 
 
 export default function ProductList() {
@@ -29,7 +30,7 @@ export default function ProductList() {
       {
         products.map(product=>(
           <Table.Row key={product.id}>
-          <Table.Cell>{product.productName}</Table.Cell>
+          <Table.Cell><Link to={`/products/${product.productName}`}>{product.productName}</Link></Table.Cell>
           <Table.Cell>{product.unitPrice}</Table.Cell>
           <Table.Cell>{product.unitsInStock}</Table.Cell>
           <Table.Cell>{product.quantityPerUnit}</Table.Cell>
